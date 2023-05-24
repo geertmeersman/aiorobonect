970d436b04027d4076b45da116092d82dda3e01e
geertmeersman <geertmeersman@users.noreply.github.com>
2023-05-24 15:41:37 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[val.strip() for val in open("requirements.txt")],
-    version="v0.1.12",
+    version="v0.1.13",
 )
