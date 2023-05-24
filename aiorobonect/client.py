997aec75fb140653b06069cc24a6353ff7d1fa20
Geert Meersman <geertmeersman+github@gmail.com>
2023-05-24 17:17:50 +0200

    Commands added and session closed when exception occurs

--- aiorobonect/client.py
+++ aiorobonect/client.py
@@ -46,7 +46,11 @@ class RobonectClient:
         """Send command to mower."""
         if command is None:
             return False
-        params = urllib.parse.urlencode(params)
+        if params is None:
+            params = ""
+        else:
+            params = urllib.parse.urlencode(params)
+
         if command == "job":
             _LOGGER.debug(f"Job params: {params}")
             return
@@ -59,6 +63,7 @@ class RobonectClient:
                 try:
                     result = await response.json(content_type=None)
                 except Exception as e:
+                    await self.session_close()
                     _LOGGER.error(e)
                     return False
                 _LOGGER.debug("Result mower data: %s", result)
@@ -86,3 +91,27 @@ class RobonectClient:
         self.sleeping = result.get("status").get("status") == 17
         await self.session_close()
         return result
+
+    async def async_start(self) -> bool:
+        """Start the mower."""
+        return await self.async_cmd("start")
+
+    async def async_stop(self) -> bool:
+        """Stop the mower."""
+        return await self.async_cmd("stop")
+
+    async def async_reboot(self) -> bool:
+        """Reboot Robonect."""
+        return await self.async_cmd("service", {"service": "reboot"})
+
+    async def async_shutdown(self) -> bool:
+        """Shutdown Robonect."""
+        return await self.async_cmd("service", {"service": "shutdown"})
+
+    async def async_sleep(self) -> bool:
+        """Make Robonect sleep."""
+        return await self.async_cmd("service", {"service": "sleep"})
+
+    def sleeping(self) -> int:
+        """Return if the mower is sleeping."""
+        return self.sleeping
